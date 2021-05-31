923302f4b885ecea7d24d218f19e64b35f220217
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2021-05-31 12:18:11 +0530

    Update up9.1.lua

--- update/up9.1.lua
+++ update/up9.1.lua
@@ -1,2 +1,11 @@
 update = "ok"
 gg.toast("groot")
+ags = gg.alert("New update available please update the script \nV10.0 link: https://mrgrootpubgm.blogspot.com/2021/05/nano-script-for-pubgm-hack.html?m=1","Copy link")
+  if ags == 1 then
+  gg.copyText("https://mrgrootpubgm.blogspot.com/2021/05/nano-script-for-pubgm-hack.html?m=1")
+  print ("copied link")
+  os.exit()
+  end
+
+
+os.exit()
